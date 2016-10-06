129f3b879cbacb5c279add57471708a581e2d2a0
skye1428 <skye1428@gmail.com>
2016-10-06 23:56:15 +0530

    Add files via upload

--- Rep.c
+++ Rep.c
@@ -6,17 +6,16 @@ void main()
 	for(i=0;i<n;i++)
 		scanf("%d",&a[i]);
 	for(i=0;i<n;i++)
-	{
-		for(j=i+1;j<n;j++)
+	{ 
+		c=0;
+		for(j=0;j<n;j++)
 			if(a[i]==a[j])
-			{
-				printf("%d",a[i]);
-				c++;
-			}
+					c=c+1;
 		if(c==1)
+		{
+			printf("%d",a[i]);	
 			break;
+	    }
     }
-    if(c==0)
-    	printf("All are unique nos");
 		 	
 }
